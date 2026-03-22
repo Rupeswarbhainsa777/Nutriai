2d0b759eedd47c647434c1eb99e2530e3c945cd7
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-23 02:57:54 +0530

    first commit in nutriai

---
+++ src/main/java/com/code/Nutriai/NutriaiApplication.java
@@ -0,0 +1,13 @@
+package com.code.Nutriai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/Nutriai/NutriaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.Nutriai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
