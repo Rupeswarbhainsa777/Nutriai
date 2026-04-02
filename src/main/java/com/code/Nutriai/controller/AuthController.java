36470f0499e7906779dbc3a5eb7226f634f8bfda
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-02 20:27:50 +0530

    addedd reg api done

--- src/main/java/com/code/Nutriai/controller/AuthController.java
+++ src/main/java/com/code/Nutriai/controller/AuthController.java
@@ -1,4 +1,35 @@
 package com.code.Nutriai.controller;
 
+import com.code.Nutriai.model.User;
+import com.code.Nutriai.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/reg")
+    public ResponseEntity<User> registration(@RequestBody  User user){
+
+
+        User user1 = authService.registration(user);
+
+        if(user1 == null){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(user1);
+
+    }
+
+
+
 }
