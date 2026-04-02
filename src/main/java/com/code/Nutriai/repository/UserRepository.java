33f982cda003bd22ade1b8fdb191f4a839640dcd
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-02 20:11:57 +0530

    user repositiry add

---
+++ src/main/java/com/code/Nutriai/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.code.Nutriai.repository;
+
+import com.code.Nutriai.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
