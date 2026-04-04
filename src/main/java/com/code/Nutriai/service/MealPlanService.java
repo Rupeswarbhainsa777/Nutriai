60556deb9b7e38041520d3bb6c089a450d218903
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-04 21:48:54 +0530

    MealPlanService add

--- src/main/java/com/code/Nutriai/service/MealPlanService.java
+++ src/main/java/com/code/Nutriai/service/MealPlanService.java
@@ -2,6 +2,7 @@ package com.code.Nutriai.service;
 
 import com.code.Nutriai.model.MealPlan;
 import com.code.Nutriai.model.MealPlanEntry;
+import com.code.Nutriai.model.Recipe;
 import com.code.Nutriai.model.User;
 import com.code.Nutriai.repository.MealPlanEntryRepository;
 import com.code.Nutriai.repository.MealPlanRepository;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Locale;
 
 @Service
@@ -21,13 +23,10 @@ public class MealPlanService {
 
     @Autowired
     private final MealPlanEntryRepository mealPlanEntryRepository;
-
     @Autowired
     private final MealPlanRepository mealPlanRepository;
-
     @Autowired
     private final UserRepository userRepository;
-
     @Autowired
     private final RecipeRepository recipeRepository;
 
@@ -47,19 +46,49 @@ public class MealPlanService {
         return mealPlanRepository.findById(id).orElseThrow(() -> new RuntimeException("Meal plan not found"));
     }
 
-    public void deleteEntry(Long mealPlanId,
-                                          String day,
-                                          MealPlanEntry.MealType mealType,
-                                          Long recipeId){
-
-        MealPlanEntry entry = mealPlanEntryRepository
-                .findByMealPlanIdAndDayAndMealType(mealPlanId,day,mealType)
-                .orElseThrow(()->new RuntimeException("Entry not found"));
-
-            mealPlanEntryRepository.delete(entry);
+    public List<MealPlanEntry> getEntries(Long mealPlanId) {
+        return mealPlanEntryRepository.findByMealPlanId(mealPlanId);
     }
 
-      // Delete full meal plan
+    public MealPlanEntry addOrUpdateEntry(Long mealPlanId,
+                                          String day,
+                                          MealPlanEntry.MealType mealType,
+                                          Long recipeId) {
+
+        MealPlan mealPlan = mealPlanRepository.findById(mealPlanId).
+                orElseThrow(() -> new RuntimeException("Meal plan not found"));
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new RuntimeException("Recipe not found"));
+
+        MealPlanEntry mealPlanEntry = mealPlanEntryRepository.findByMealPlanIdAndDayAndMealType(mealPlanId, day, mealType)
+                .orElse(null);
+
+        if (mealPlanEntry == null) {
+            mealPlanEntry = new MealPlanEntry();
+            mealPlanEntry.setMealPlan(mealPlan);
+            mealPlanEntry.setDay(day);
+            mealPlanEntry.setMealType(mealType);
+        }
+        mealPlanEntry.setRecipe(recipe);
+        return mealPlanEntryRepository.save(mealPlanEntry);
+
+    }
+
+
+    public void deleteEntry(Long mealPlanId,
+                            String day,
+                            MealPlanEntry.MealType mealType,
+                            Long recipeId) {
+
+        MealPlanEntry entry = mealPlanEntryRepository
+                .findByMealPlanIdAndDayAndMealType(mealPlanId, day, mealType)
+                .orElseThrow(() -> new RuntimeException("Entry not found"));
+
+        mealPlanEntryRepository.delete(entry);
+    }
+
+
+    // Delete full meal plan
     private void deleteMealPlan(Long id) {
         mealPlanRepository.deleteById(id);
     }
