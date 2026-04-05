d178b156b7554897613804f8beac0af9b818bf45
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-06 03:12:06 +0530

    Fitness Data model update

--- src/main/java/com/code/Nutriai/model/FitnessData.java
+++ src/main/java/com/code/Nutriai/model/FitnessData.java
@@ -1,10 +1,9 @@
 package com.code.Nutriai.model;
 
-
-
 import jakarta.persistence.*;
-import lombok.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Data
@@ -18,15 +17,25 @@ public class FitnessData {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDate date;
-
-    private int steps;
-
-    private double caloriesBurned;
-
-    private String workoutType;
-
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    private LocalDate date;
+    private int steps;
+    private int caloriesBurned;
+    private int activeMinutes;
+
+    @Enumerated(EnumType.STRING)
+    private WorkoutType workoutType;
+
+    public enum WorkoutType {
+        RUNNING,
+        WALKING,
+        CYCLING,
+        SWIMMING,
+        YOGA,
+        STRENGTH,
+        OTHER
+    }
 }
