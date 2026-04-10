a5f15cfa313e84635d11bc10fd6bf80bacee4ef0
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-10 22:41:46 +0530

    MyUserDetailsService add

---
+++ src/main/java/com/code/Nutriai/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.code.Nutriai.security;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+
+    private final MyUserDetailsService myUserDetailsService;
+}
