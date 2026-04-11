307b505cf487a30adb11e3a75320a7c91e5e09dd
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-11 20:04:37 +0530

    MyUserDetailsService add

--- src/main/java/com/code/Nutriai/service/MyUserDetailsService.java
+++ src/main/java/com/code/Nutriai/service/MyUserDetailsService.java
@@ -1,7 +1,32 @@
 package com.code.Nutriai.service;
 
+import com.code.Nutriai.model.User;
+import com.code.Nutriai.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.nio.file.attribute.UserPrincipal;
 
 @Service
-public class MyUserDetailsService {
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email);
+
+        if(user==null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserPrincipal(user);
+    }
 }
