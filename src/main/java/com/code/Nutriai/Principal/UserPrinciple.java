c4d528414172848c9e8ece980ae1132558d33b49
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-05-07 14:32:28 +0530

    User principle is fixed for now but requir ro wok on it

--- src/main/java/com/code/Nutriai/Principal/UserPrinciple.java
+++ src/main/java/com/code/Nutriai/Principal/UserPrinciple.java
@@ -13,5 +13,44 @@ import java.util.List;
 public class UserPrinciple implements UserDetails {
 
 
+    private User user;
 
+    public UserPrinciple(User user){
+        this.user=user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
