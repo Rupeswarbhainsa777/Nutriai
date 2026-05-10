0cb4e11a2ca996ec75a819ba5aea8f48653dd5b9
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-05-11 02:32:38 +0530

    get all user api working

--- src/main/java/com/code/Nutriai/controller/UserController.java
+++ src/main/java/com/code/Nutriai/controller/UserController.java
@@ -1,12 +1,26 @@
 package com.code.Nutriai.controller;
 
 
+import com.code.Nutriai.model.User;
+import com.code.Nutriai.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> user(){
+
+        return  userService.allUsers();
+    }
 }
--- src/main/java/com/code/Nutriai/service/UserService.java
+++ src/main/java/com/code/Nutriai/service/UserService.java
@@ -1,8 +1,27 @@
 package com.code.Nutriai.service;
 
 
+import com.code.Nutriai.model.User;
+import com.code.Nutriai.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+
+
+    @Autowired
+    private UserRepository repository;
+
+
+    public ResponseEntity<List<User>> allUsers(){
+        List<User> allUser = repository.findAll();
+
+        return ResponseEntity.ok(allUser);
+    }
+
+
 }
