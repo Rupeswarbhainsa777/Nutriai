7ac99156b90ba73e3e94fe555787adbcd4cf31b0
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-05-11 02:19:06 +0530

    User Service Class add

--- src/main/java/com/code/Nutriai/controller/UserController.java
+++ src/main/java/com/code/Nutriai/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.code.Nutriai.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+
 }
