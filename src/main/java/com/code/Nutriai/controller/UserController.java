564f5a543a533aefc3c7d28ec2fc7b9bb6ade7d9
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-05-11 03:49:20 +0530

    some issue in update api but delete api working

--- src/main/java/com/code/Nutriai/controller/UserController.java
+++ src/main/java/com/code/Nutriai/controller/UserController.java
@@ -5,10 +5,7 @@ import com.code.Nutriai.model.User;
 import com.code.Nutriai.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,14 +18,23 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<User>> users(){
+    public ResponseEntity<List<User>> users() {
 
-        return  userService.allUsers();
+        return userService.allUsers();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<User>> user(@PathVariable long id){
+    public ResponseEntity<Optional<User>> user(@PathVariable Long id) {
         return userService.user(id);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.update(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
     }
 }
--- src/main/java/com/code/Nutriai/service/UserService.java
+++ src/main/java/com/code/Nutriai/service/UserService.java
@@ -18,17 +18,42 @@ public class UserService {
     private UserRepository repository;
 
 
-    public ResponseEntity<List<User>> allUsers(){
+    public ResponseEntity<List<User>> allUsers() {
         List<User> allUser = repository.findAll();
 
         return ResponseEntity.ok(allUser);
     }
 
-    public ResponseEntity<Optional<User>> user(long id){
+    public ResponseEntity<Optional<User>> user(Long id) {
         Optional<User> user = repository.findById(id);
 
         return ResponseEntity.ok(user);
     }
 
+    public ResponseEntity<User> update(Long id, User user) {
+
+        User existingUser = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+        existingUser.setName(user.getName());
+        existingUser.setPassword(user.getPassword());
+        existingUser.setGoal(user.getGoal());
+        existingUser.setDietaryRestrictions(user.getDietaryRestrictions());
+        existingUser.setHeight(user.getHeight());
+        existingUser.setWeight(user.getWeight());
+        existingUser.setAge(user.getAge());
+        existingUser.setMealPlans(user.getMealPlans());
+
+        User update = repository.save(existingUser);
+
+        return ResponseEntity.ok(update);
+    }
+
+    public ResponseEntity<String> deleteUser(Long id) {
+        User existingUser = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+
+        repository.delete(existingUser);
+        return ResponseEntity.ok("User deleted successfully with id: " + id);
+    }
 
 }
