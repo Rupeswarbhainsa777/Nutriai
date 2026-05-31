b66587f6c70f031f990eccb5a4635c683d016baa
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-05-31 11:12:03 +0530

    Remove .env from repository

--- src/main/java/com/code/Nutriai/config/SecurityConfig.java
+++ src/main/java/com/code/Nutriai/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.code.Nutriai.connfig;
+package com.code.Nutriai.config;
 
 
 import com.code.Nutriai.Filters.JwtFilter;
@@ -7,17 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -45,7 +40,7 @@ public class SecurityConfig {
 
                 .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
+//
         return security.build();
     }
 
