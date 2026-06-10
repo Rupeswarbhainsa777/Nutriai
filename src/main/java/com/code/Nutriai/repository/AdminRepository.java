7f0738cdedda84cf0df0a149d2f6bf49ff25ec0f
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-06-10 23:09:04 +0530

    admin repo add

---
+++ src/main/java/com/code/Nutriai/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package com.code.Nutriai.repository;
+
+import com.code.Nutriai.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+}
